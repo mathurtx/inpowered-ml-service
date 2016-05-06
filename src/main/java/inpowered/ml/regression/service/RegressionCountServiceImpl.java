216bbd9930b0faa4dc9b8a9dbe80fe8ce1b8e19e
Tanmay Mathur <t.mathur@elsevier.com>
2016-05-06 12:34:55 -0400

    Calling mock method

--- src/main/java/inpowered/ml/regression/service/RegressionCountServiceImpl.java
+++ src/main/java/inpowered/ml/regression/service/RegressionCountServiceImpl.java
@@ -10,8 +10,8 @@ public class RegressionCountServiceImpl implements RegressionCountService {
 
 	public Integer getViewCount(String link, String title, String summary, String authorActorId, String pubDate) throws Exception {
 		PythonInterpreter interpreter = new PythonInterpreter();
-		interpreter.execfile("//Users/mathurtx/Documents/inpowered/Prediction.py");
-		PyObject getPrediction = interpreter.get("predictor");
+		interpreter.execfile("//Users/mathurtx/Documents/inpowered/predictor.py");
+		PyObject getPrediction = interpreter.get("predictormock");
 		PyObject result = getPrediction.__call__(new PyString(authorActorId),new PyString(summary));
 		String realResult = (String) result.__tojava__(String.class);
 		return Integer.parseInt(realResult);
